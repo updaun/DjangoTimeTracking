7d4b1be8c10ac77218fd0047dd24fa5c5f1af5ed
Up & Daun <82289435+updaun@users.noreply.github.com>
2021-09-24 18:11:29 +0900

    part10

--- apps/team/models.py
+++ apps/team/models.py
@@ -3,6 +3,16 @@ from django.db import models
 
 # Create your models here.
 
+class Plan(models.Model):
+    title = models.CharField(max_length=255)
+    max_projects_per_team = models.IntegerField(default=0)
+    max_members_per_team = models.IntegerField(default=0)
+    max_tasks_per_project = models.IntegerField(default=0)
+    price = models.IntegerField(default=0)
+    is_default = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
 
 class Team(models.Model):
     #
@@ -17,6 +27,18 @@ class Team(models.Model):
     )
 
     #
+    # Plan status
+
+    PLAN_ACTIVE = 'active'
+    PLAN_CANCELED = 'canceled'
+
+    CHOICES_PLAN_STATUS = (
+        (PLAN_ACTIVE, 'Active'),
+        (PLAN_CANCELED, 'Canceled')
+    )
+
+
+    #
     # Fields
 
     title = models.CharField(max_length=255)
@@ -24,6 +46,12 @@ class Team(models.Model):
     created_by = models.ForeignKey(User, related_name='created_teams', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=10, choices=CHOICES_STATUS, default=ACTIVE)
+
+    plan = models.ForeignKey(Plan, related_name='teams', on_delete=models.CASCADE)
+    plan_and_date = models.DateTimeField(blank=True, null=True)
+    plan_status = models.CharField(max_length=20, choices=CHOICES_PLAN_STATUS, default=PLAN_ACTIVE)
+    stripe_customer_id = models.CharField(max_length=255, blank=True, null=True)
+    stripe_subscription_id = models.CharField(max_length=255, blank=True, null=True)
 
     class Meta:
         ordering = ['title']
--- apps/team/urls.py
+++ apps/team/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import add, team, edit, activate_team, invite
+from .views import add, team, edit, activate_team, invite, plans
 
 app_name = 'team'
 
@@ -8,6 +8,7 @@ urlpatterns = [
     path('add/', add, name='add'),
     path('edit/', edit, name='edit'),
     path('invite/', invite, name='invite'),
+    path('plans/', plans, name='plans'),
     path('activate_team/<int:team_id>', activate_team, name='activate_team'),
     path('<int:team_id>/', team, name='team')
 ]
